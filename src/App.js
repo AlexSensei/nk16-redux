1a1223f85392e0b1f181ac148f9134d95826856e
AlexSensei <antishadow15@gmail.com>
2022-04-05 21:35:05 +0200

    Zadatak 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieSearch from "./components/MovieSearch";
 
 function App() {
   return (
     <div className="App">
+      <MovieSearch />
       <MovieList />
     </div>
   );
