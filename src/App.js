84fe3589fabf583f6e818456bc46c6cdaeae2ed7
AlexSensei <antishadow15@gmail.com>
2022-04-05 21:52:54 +0200

    Zadatak 7

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieSearch from "./components/MovieSearch";
+import { useSelector } from "react-redux";
+import { selectedMoviesCount } from "./store/movie/selectors";
 
 function App() {
+  const count = useSelector(selectedMoviesCount);
+
   return (
     <div className="App">
       <MovieSearch />
+      <p>Selektovano je {count} filma</p>
       <MovieList />
     </div>
   );
