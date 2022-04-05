dd09d0e74094ee508ec71a214b3c90fa56170372
AlexSensei <antishadow15@gmail.com>
2022-04-05 21:18:13 +0200

    Zadatak 2&3

---
+++ src/components/MovieList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import movieService from "../services/MovieService";
+import { selectMovies } from "../store/movie/selectors";
+import { setMovies } from "../store/movie/slice";
+
+const MovieList = (props) => {
+  const dispatch = useDispatch();
+
+  const movies = useSelector(selectMovies);
+  const handleSetMovies = (movies) => dispatch(setMovies(movies));
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    const data = await movieService.getAll();
+
+    handleSetMovies(data);
+  };
+
+  return <div>MovieList</div>;
+};
+
+export default MovieList;
